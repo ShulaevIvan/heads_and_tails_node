d7f3f0af6d7896de2c5867c491a9269fdabd5d95
= <ivnshulaev@gmail.com>
2024-05-02 16:48:29 +0300

    add some fixes

--- analyzer.js
+++ analyzer.js
@@ -1,8 +1,17 @@
 'use strict';
-
+const fs = require('fs');
+const path = require('path');
 
 const analyze = (logPath) => {
-    console.log(logPath)
+    if(fs.existsSync(logPath)) {
+        const jsonLog = require(logPath);
+        const allRounds = jsonLog.reduce((acc, item) => acc += 1, 0);
+        const playerWin = jsonLog.filter((item) => item.winner === 'Player').length;
+        const winComp = jsonLog.filter((item) => item.winner === 'Comp').length;
+        const winPercent=  Number((playerWin / (playerWin + winComp) * 100).toFixed(2));
+        const text = `stats:  All Rounds- ${allRounds},\n Player Win - ${playerWin},\n Comp Win - ${winComp},\n Player Win Percent - ${winPercent}\n`;
+        console.log(text);
+    }
 };
 
 module.exports = analyze;
